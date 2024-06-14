1a2c253a4d0fb1507fb0c03b9e82e092301db210
Pedro Henrique <144647475+devpeu@users.noreply.github.com>
2024-06-14 20:34:22 -0300

    Correção

--- leitoris/src/main/java/com/nassau/Biblioteca/repositories/BookRepository.java
+++ leitoris/src/main/java/com/nassau/Biblioteca/repositories/BookRepository.java
@@ -1,9 +1,1 @@
-package com.nassau.Biblioteca.repositories;
 
-import com.nassau.leitoris.models.BookModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<BookModel, Long> {
-}
